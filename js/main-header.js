dc904aee2988d09e68e42c5cf436810eeffcd25c
hunimoda <hunimoda@gmail.com>
2021-06-26 01:41:03 +0900

    3. added util-menu with animation

--- js/main-header.js
+++ js/main-header.js
@@ -6,13 +6,28 @@ const more = document.querySelector('.bottom__more');
 const moreSpan = more.querySelector('span');
 const moreNav = more.querySelector('.bottom__navbar');
 
+const openUtilMenuBtn = document.querySelector('.top-bar__navbar .open-util-menu');
+const utilMenu = document.querySelector('.util-menu');
+const exitUtilMenuBtn = utilMenu.querySelector('.util-menu__bar .util-menu__exit-btn');
+
 function onMouseInOutMore() {
     moreSpan.classList.toggle(HIDDEN_CLASS);
     moreNav.classList.toggle(HIDDEN_CLASS);
 }
 
+function onUtilOpenClick() {
+    utilMenu.classList.remove(HIDDEN_CLASS);
+}
+
+function onUtilCloseClick() {
+    utilMenu.classList.add(HIDDEN_CLASS);
+}
+
 function mainApp() {
     more.addEventListener('mouseenter', onMouseInOutMore);
     more.addEventListener('mouseleave', onMouseInOutMore);
+
+    openUtilMenuBtn.addEventListener('click', onUtilOpenClick);
+    exitUtilMenuBtn.addEventListener('click', onUtilCloseClick);
 }
 mainApp();
